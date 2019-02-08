a28077390fa75d1658c609c371d5e938d484b53f
AbdelfatahAtef <abdel.fattah@tajawal.com>
2019-02-08 14:25:17 +0200

    Adding constants file

---
+++ src/constants.js
@@ -0,0 +1,31 @@
+export default {
+    colors: ['#FFF', '#8AC2E4', '#9DB179', '#DDEEF2', '#EF756A', '#0F7369', '#FFCC41', '#D3E1FF', '#015ED1', '#F78BA1', '#214778', '#D39643', '#F5C130', '#70372C'],
+    fontTypes: ['Roboto', 'ZCOOL KuaiLe', 'Titillium Web', 'Indie Flower', 'Lobster', 'Shadows Into Light', 'Amatic SC', 'Gloria Hallelujah'],
+    tweetsFontSizes: ['18px', '20px', '22px'],
+    users: [
+        {
+            image: 'http://lorempixel.com/400/200/',
+            name: '@BoboOo__',
+            userName: 'BoboOo',
+            numOfTweets: '14.2k',
+            numOfFollowers: '292',
+            numOfFollowing: '618',
+        },
+        {
+            image: 'http://lorempixel.com/400/200/',
+            name: '@omar',
+            userName: 'Omar Moustafa',
+            numOfTweets: '14.2k',
+            numOfFollowers: '292',
+            numOfFollowing: '618',
+        },
+        {
+            image: 'http://lorempixel.com/400/200/',
+            name: '@moza',
+            userName: 'Mahmoud Moza',
+            numOfTweets: '14.2k',
+            numOfFollowers: '292',
+            numOfFollowing: '618',
+        }
+    ]
+}
