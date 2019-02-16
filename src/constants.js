aa2902ae71c94740ed6d8dbf839af7bb3a3f50a5
AbdelfatahAtef <abdel.fattah@tajawal.com>
2019-02-16 14:27:26 +0200

    Add new color for constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export default {
-    colors: ['#FFF', '#8AC2E4', '#9DB179', '#DDEEF2', '#EF756A', '#0F7369', '#FFCC41', '#D3E1FF', '#015ED1', '#F78BA1', '#214778', '#D39643', '#F5C130', '#70372C'],
+    colors: ['#FFF', '#8AC2E4', '#9DB179', '#DDEEF2', '#EF756A', '#0F7369', '#FFCC41', '#D3E1FF', '#015ED1', '#F78BA1', '#214778', '#D39643', '#F5C130', '#70372C', '#000'],
     fontTypes: ['Roboto', 'ZCOOL KuaiLe', 'Titillium Web', 'Indie Flower', 'Lobster', 'Shadows Into Light', 'Amatic SC', 'Gloria Hallelujah'],
     tweetsFontSizes: ['18px', '20px', '22px'],
     users: [
@@ -213,5 +213,5 @@ export default {
                 },
             ]
         }
-    ]
+    ],
 }
